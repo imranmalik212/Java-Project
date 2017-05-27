90c094d45148e7103fb2f0804846c4351eae0ad5
Imran Malik <iakhtar2001@gmail.com>
2017-05-27 12:03:18 -0400

    pls review my code

---
+++ chapter1/Aamirbc.java
@@ -0,0 +1,11 @@
+// - Single Line Comment
+/* Multi line comment */
+
+public class Aamirbc
+{
+
+	public static void main (String args[])
+	{
+	System.out.println("Aamir bc!");
+	}
+}
---
+++ chapter1/HelloWorldClass.java
@@ -0,0 +1,11 @@
+// - Single Line Comment
+/* Multi line comment */
+
+public class HelloWorldClass
+{
+
+	public static void main (String args[])
+	{
+	System.out.println("Hellow World!");
+	}
+}
